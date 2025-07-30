d45b9c55bfbf69aaee327bb48f8295333ad39d1b
Proberto556estacio <202507269518@alunos.estacio.br>
2025-07-30 10:40:49 +0000

    Criado o printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,8 @@
 //Teste larissa
 
 int main() {
+    print("Desafio Super Trunfo - Paises \n");
+        
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     

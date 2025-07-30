55e45018d24b49a8352a00ff2e2507643568f6c0
Proberto556estacio <202507269518@alunos.estacio.br>
2025-07-30 11:06:22 +0000

    Novo commit

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -8,6 +8,8 @@
 
 int main() {
     print("Desafio Super Trunfo - Paises \n");
+    printf("Novo commit\n");
+    
         
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.

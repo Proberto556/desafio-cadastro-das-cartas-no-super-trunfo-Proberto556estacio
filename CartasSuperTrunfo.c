044259d7db29b9723ecdc57d45c2f35950f1c372
Proberto556estacio <202507269518@alunos.estacio.br>
2025-08-02 19:09:46 +0000

    Após orientação: Tipo de Dados para "Estado"

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,10 +6,23 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
- 
-int main() {
+
+// Sugestão: Defina variáveis separadas para cada atributo da cidade.
+    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+    
+    // Cadastro das Cartas:
+    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
+    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
+    
+    // Exibição dos Dados das Cartas:
+    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
+    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+
+
+    int main() {
     // Variáveis definidas - Super Trunfo - Países 
-    char estado1, estado2;
+    char estado1[3], estado2[3];
     char codcar1[4], codcar2[4];
     char nomecid1[40], nomecid2[40]; 
     int populacao1, populacao2;
@@ -20,11 +33,22 @@ int main() {
     // Título para exibição do projeto
     printf("Desafio Super Trunfo - Paises \n\n");
 
+    // Definição dos Estados x Letras
+    printf("Letras definidas para cada Estado:\n\n");
+    printf("A = Alagoas \n");
+    printf("B = Bahia \n");
+    printf("C = Espirito Santo \n");
+    printf("D = Minas Gerais \n");
+    printf("E = São Paulo \n");
+    printf("F = Rio de Janeiro\n");
+    printf("G = Sergipe \n");
+    printf("H = Roraima \n\n");
+    
     // Entrada de dados - Carta1
     printf("Digite abaixo as informações para Carta1\n");
 
-    printf("Uma letra de 'A' a 'H' (representando um Estado): \n");
-    scanf(" %c", &estado1);
+    printf("Uma letra de 'A' a 'H' representando um Estado: \n");
+    scanf(" %s", estado1);
 
     printf("A letra que representa o Estado seguida de um número entre 01 a 04 (ex: C02, F04): \n");
     scanf("%3s", codcar1);
@@ -46,7 +70,7 @@ int main() {
  
     // Saida de dados - Carta1
     printf("Carta1: \n");
-    printf("Estado: %c \n", estado1);
+    printf("Estado: %s \n", estado1);
     printf("Código: %3s \n", codcar1);
     printf("Nome da Cidade: %s \n", nomecid1);
     printf("População: %i \n", populacao1);
@@ -57,8 +81,8 @@ int main() {
     // Entrada de dados - Carta2
     printf("Digite abaixo as informações para Carta2 \n");
     
-    printf("Uma letra de 'A' a 'H' (representando um Estado): \n"); 
-    scanf(" %c", &estado2); 
+    printf("Uma letra de 'A' a 'H' representando um Estado: \n"); 
+    scanf(" %s", estado2); 
 
     printf("A letra que representa o Estado seguida de um número entre 01 a 04 (ex: C02, F04): \n");
     scanf("%s", codcar2);
@@ -80,28 +104,15 @@ int main() {
 
     //Saida de dados - Carta2
     printf("Carta2 \n");
-    printf("Estado: %c \n", estado2);
+    printf("Estado: %s \n", estado2);
     printf("Código: %s \n", codcar2);
     printf("Nome da Cidade: %s \n", nomecid2);
     printf("População: %i \n", populacao2);
     printf("Área (em Km²): %.2f \n", area2);
     printf("PIB: %.2f bilhões de reais \n", pib2);
     printf("Número de Pontos Turísticos: %i \n", ponto2);
-
     
-
-
-        
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
+      
     
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
-
     return 0;
 }

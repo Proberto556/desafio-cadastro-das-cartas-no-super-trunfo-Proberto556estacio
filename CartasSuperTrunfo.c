13397125c4101ed225268ae280b152242e48a3d7
Proberto556estacio <202507269518@alunos.estacio.br>
2025-08-15 15:41:45 +0000

    Ajuste no resultado da comparação Carta1 ou Carta2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -62,7 +62,7 @@
     printf("Digite as siglas do Estado (ex: MG, SP): \n");
     scanf(" %2s", estado1);
 
-    printf("A letra que representa o Estado seguida de um número entre 01 a 04 (ex: C02, F04): \n");
+    printf("A letra que representa o Estado seguida de um número entre 1 a 4 (ex: DF2, CE4): \n");
     scanf("%3s", codcar1);
 
     printf("O nome da cidade: \n");
@@ -83,26 +83,28 @@
     //Cálculo da Densidade populacional carta1 (Utilizando conversão explicita e proteção Contra divisão por Zero) - Nível Aventureiro
     
         if (area1 != 0.0f) {
-            densipop1 =(float) populacao1 / area1;
-        } else {
-            densipop1 = 0.0f; //Define um valor padrão seguro
+            float densipop1 = (float) populacao1 / area1;
+        }   else {
+            float densipop1 = 0.0f; //Define um valor padrão seguro
             printf("Atenção: Área da Carta1 é zero. Densidade populacional definida como 0.\n");
         }
+
 
     //Cálculo Pib per Capita carta1 (Utilizando conversão explicita e proteção Contra divisão por Zero) - Nível Aventureiro
     
         if (populacao1 != 0.0f) {
-        pibper1 = pib1 * 1000000000.0 / (float) populacao1;
-        } else {
-            pibper1 = 0.0f; //Define um valor padrão seguro
+            float pibper1 = pib1 * 1000000000.0 / (float) populacao1;
+        }   else {
+            float pibper1 = 0.0f; //Define um valor padrão seguro
             printf("Atenção: População da carta 1 é zero. Pib per Capita definido como 0\n");        
         } 
     
+        
     //Cálculo inverso_densidade1 - Superpoder Carta1 - Nível Mestre 
-    float inverso_densidade1 = 0.0f;
+        float inverso_densidade1 = 0.0f;
         if (densipop1 != 0.0f) {
             inverso_densidade1 = 1.0f / densipop1;    
-        } else {
+        }   else {
             // Pode-se definir um valor padrão para o inverso da densidade quando a densidade é zero,
             // ou simplesmente manter 0.0f, dependendo da regra do jogo.
             // Ex: inverso_densidade1 = valor_muito_alto_ou_0;
@@ -111,7 +113,7 @@
 
     //Cálculo Superpoder das Carta1 - Nível Mestre  
     float superpoder1 = (float) populacao1 +  area1 + pib1 + (float) ponto1 + inverso_densidade1 + pibper1; 
-    printf("Superpoder Carta1 = %f \n", superpoder1);
+    printf("Superpoder Carta1 = %.2f \n", superpoder1);
      
     // Saida de dados - Carta1
     printf("Carta1: \n");
@@ -124,16 +126,14 @@
     printf("Número de Pontos Turísticos: %d \n", ponto1);
     printf("Densidade populacional: %.2f hab/km² \n", densipop1);
     printf("PIB per Capita: %.2f reais \n\n", pibper1);
-
-    
-      
+          
     //Entrada de dados - Carta2
     printf("Digite abaixo as informações para Carta2 \n");
     
     printf("Digite as siglas do Estado (ex: MG, SP): \n"); 
     scanf(" %2s", estado2); 
 
-    printf("A letra que representa o Estado seguida de um número entre 01 a 04 (ex: C02, F04): \n");
+    printf("A letra que representa o Estado seguida de um número entre 1 a 4 (ex: DF2, CE4): \n");
     scanf("%3s", codcar2);
 
     printf("O nome da cidade: \n");
@@ -154,9 +154,9 @@
         //Cálculo da Densidade populacional carta2 (Utilizando conversão explicita e proteção Contra divisão por Zero) - Nível Aventureiro
     
         if (area2 != 0.0f) {
-            densipop2 =(float) populacao2 / area2;
-        } else {
-            densipop2 = 0.0f; //Define um valor padrão seguro
+            float densipop2 =(float) populacao2 / area2;
+        }   else {
+            float densipop2 = 0.0f; //Define um valor padrão seguro
             printf("Atenção: Área da Carta 2 é zero. Densidade populacional definida como 0.\n");
         }
 
@@ -164,24 +164,29 @@
         //Cálculo Pib per Capita carta2 (Utilizando conversão explicita e proteção Contra divisão por Zero) - Nível Aventureiro
     
         if (populacao2 != 0.0f) {
-        pibper2 = pib2 * 1000000000.0 / (float) populacao2;
-        } else {
-            pibper2 = 0.0f; //Define um valor padrão seguro
+            float pibper2 = pib2 * 1000000000.0 / (float) populacao2;
+        }   else {
+            float pibper2 = 0.0f; //Define um valor padrão seguro
             printf("Atenção: População da carta 2 é zero. Pib per Capita definido como 0\n");        
         } 
 
+
         //Cálculo inverso_densidade2 - Superpoder Carta1 - Nível Mestre 
-    float inverso_densidade2 = 0.0f;
+        float inverso_densidade2 = 0.0f;
         if (densipop2 != 0.0f) {
             inverso_densidade2 = 1.0f / densipop2;}
-        else {
+            else {
             //Pode-se definir um valor padrão para o inverso da densidade quando a densidade é zero,
             //ou simplesmente manter 0.0f, dependendo da regra do jogo.
             //Ex: inverso_densidade2 = valor_muito_alto_ou_0;}
         }  
-   
     
-        //Saida de dados - Carta2
+
+    //Cálculo Superpoder das Carta2 - Nível Mestre  
+    float superpoder2 = (float) populacao2 +  area2 + pib2 + (float) ponto2 + inverso_densidade2 + pibper2; 
+    printf("Superpoder Carta2 = %.2f \n\n", superpoder2);
+   
+    //Saida de dados - Carta2
     printf("Carta2 \n");
     printf("Estado: %s \n", estado2);
     printf("Código: %3s \n", codcar2);
@@ -191,13 +196,8 @@
     printf("PIB: %.2f bilhões de reais \n", pib2);
     printf("Número de Pontos Turísticos: %d \n", ponto2);
     printf("Densidade Populacional: %.2f hab/km² \n", densipop2);
-    printf("PIB per Capita: %.2f reais\n\n", pibper2);
-
-    
-    //Cálculo Superpoder das Carta2 - Nível Mestre 
-    float superpoder2 = (float) populacao2 + area2 + pib2 + (float) ponto2 + inverso_densidade2 + pibper2; 
-    printf("Superpoder Carta2 = %f \n", superpoder2);
-    
+    printf("PIB per Capita: %.2f reais\n\n", pibper2); 
+   
     
     //Cálculo comparação das Cartas 1 e 2 - Nível Mestre
     resultadoPopulacao = populacao1 > populacao2;
@@ -208,17 +208,25 @@
     resultadoPibpercapita = pibper1 > pibper2;
     resultadoSuperpoder = superpoder1 > superpoder2;
     
-    //Exibir comparação das cartas 1 e 2 -Nível Mestre
+    //Exibir valores comparação das cartas 1 e 2 -Nível Mestre
     printf("Comparação de Cartas:\n");
     printf("População           : Carta1 é %lu          - Carta2 é %lu          - Resultado vencedor %d \n", populacao1, populacao2, resultadoPopulacao);
     printf("Área1               : Carta1 é %.2f         - Carta2 é %.2f         - Resultado vencedor %d\n", area1, area2, resultadoArea);
     printf("Pib1                : Carta1 é %.2f         - Carta2 é %.2f         - Resultado vencedor %d\n", pib1, pib2, resultadoPib);
     printf("Pontos Turísticos   : Carta1 é %d           - Carta2 é %d           - Resultado vencedor: %d \n", ponto1, ponto2, resultadoPonto);
-    printf("Densidade Populacional: Carta1 é %.2f       - Carta2 é %.2f         - Resultado vencedor %d\n", densipop1, densipop2, resultadoDensipop);
-    printf("Pib per Capita1     : Carta1 é %.2f         - Carta2 é %.2f         - Resultado vencedor %d\n", pibper1, pibper2, resultadoPibpercapita);
-    printf("Superpoder          : Carta1 é %.2f         - Carta2 é %.2f           - Resultado vencedor %d\n", superpoder1, superpoder2, resultadoSuperpoder);
-       
+    printf("Densidade Populacional: Carta1 é %f       - Carta2 é %f         - Resultado vencedor %d\n", densipop1, densipop2, resultadoDensipop);
+    printf("Pib per Capita      : Carta1 é %.2f         - Carta2 é %.2f         - Resultado vencedor %d\n", pibper1, pibper2, resultadoPibpercapita);
+    printf("Superpoder          : Carta1 é %.2f         - Carta2 é %.2f           - Resultado vencedor %d\\n", superpoder1, superpoder2, resultadoSuperpoder);
 
+    //Exibir resultado se Carta1 ou Carta2 venceu
+    printf("População: %s venceu (%d)\n", resultadoPopulacao? "Carta1" : "Carta2", resultadoPopulacao);
+    printf("Área: %s venceu (%d)\n", resultadoArea? "Carta1" : "Carta2", resultadoArea);
+    printf("Pib: %s venceu (%d)\n", resultadoPib? "Carta1" : "Carta2)", resultadoPib);
+    printf("Pontos Turísticos: %s venceu (%d)\n", resultadoPonto? "Carta1" : "Carta2", resultadoPonto);
+    printf("Densidade Populacional: %s venceu (%d)\n", resultadoDensipop? "Carta1" : "Carta2", resultadoDensipop);
+    printf("Pib per Capida: %s venceu (%d)\n", resultadoPibpercapita? "Carta1" : "Carta", resultadoPibpercapita);
+    printf("Superpoder: %s venceu (%d)\n", resultadoSuperpoder? "Carta1" : "Carta2", resultadoSuperpoder);
+    
     
     return 0;
 }
